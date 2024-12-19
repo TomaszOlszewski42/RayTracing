f025c32342e2e0531e4356b53d3d5fd0dc20cc87
TomaszOlszewski42 <tomaszolszewski42@gmail.com>
2024-12-20 00:04:56 +0100

    First image

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace RayTracing
+﻿namespace RayTracing;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int imageHeight = 256;
+        int imageWidth = 256;
+
+        var file = File.Create(".\\..\\..\\..\\image.ppm");
+        var output = new StreamWriter(file);
+
+        output.WriteLine($"P3\n{imageWidth} {imageHeight}\n255");
+
+        for(int i = 0; i < imageHeight; i++)
         {
-            Console.WriteLine("Hello, World!");
+            for (int j = 0; j < imageWidth; j++) 
+            {
+                var r = (double)i / (imageWidth - 1);
+                var g = (double)j / (imageHeight - 1);
+                var b = 0.0;
+
+                int ir = (int)(255.999 * r);
+                int ig = (int)(255.999 * g);
+                int ib = (int)(255.999 * b);
+
+                output.WriteLine($"{ir} {ig} {ib}");
+            }
         }
+        
     }
 }
